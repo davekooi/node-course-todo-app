d07a8cd2ce43a1e7ddbea21d2991d27edc5b1199
David Kooistra <dkooi@umich.edu>
2018-08-09 23:36:42 -0400

    Initial commit

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,10 @@
+const {MongoClient, ObjectId} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to mongodb server');
+  }
+  console.log('Connected to mongodb server');
+
+  db.close();
+});
